19f011403dcabc38123cfa24082ec28c26909fd7
lizhaoming <38341508+LiMingCoding@users.noreply.github.com>
2021-06-21 19:46:41 +0800

    再次测试一下上传

---
+++ 垃圾回收/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 垃圾回收
+{
+    class Person
+    {
+       public int Age
+        {
+            set;
+            get;
+        }
+       public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
