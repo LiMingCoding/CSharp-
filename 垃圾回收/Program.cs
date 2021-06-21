cc1376557996a20951527aa4240c01a7c422d58f
lizhaoming <38341508+LiMingCoding@users.noreply.github.com>
2021-06-21 19:46:41 +0800

    新建垃圾回收项目
    测试一下上传功能

---
+++ 垃圾回收/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 垃圾回收
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
